80701c352c682c102438576839bae36fe75c343d
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-12-23 18:35:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://VijayTG:aaaa1111@cluster0.tpd9c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "VijayTG)
+DATABASE_NAME = environ.get('DATABASE_NAME', "VijayTG")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
