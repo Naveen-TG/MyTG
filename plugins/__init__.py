a5f6c3793ff8bb0f02b91ea7be2ebbd44be6d763
Naveen <93958628+Naveen-TG@users.noreply.github.com>
2022-12-24 20:22:19 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -2,3 +2,8 @@ from pyrogram import filters
 import time, os
 from aiohttp import ClientSession
 from telegraph import Telegraph
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+from telethon import TelegramClient
+import logging
+
+OWNER_ID = environ('OWNER_ID' , '')
